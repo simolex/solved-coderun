f14213a8b70e7f470a0b98d73d73d8a5a6f97709
simolex <simolex@yandex.ru>
2023-05-25 22:55:40 +0300

    add 201

--- easy/241.easy.js
+++ easy/241.easy.js
@@ -1,5 +1,8 @@
-exports.createCountdown = (n) => {
-    let value = n + 1;
+const createCountdown = function (n) {
+    let value = 0;
+    if (Number.isInteger(n) && n > 0) {
+        value = n + 1;
+    }
 
     return () => {
         if (value === 0) return 0;
