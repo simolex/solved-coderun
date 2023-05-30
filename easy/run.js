3fd3de093675d4c3a87c80d781d8b39f87fee1de
simolex <simolex@yandex.ru>
2023-05-31 00:37:14 +0300

    starting 170

--- easy/run.js
+++ easy/run.js
@@ -1,7 +1,7 @@
 const merge = require("./170.easy.js");
 
-const num1 = [90, 80, 29, 18, 0, 0, 0];
-const num2 = [88, 55, 46];
+const num1 = [90, 80, 29, 18, 0];
+const num2 = [88];
 
 merge(num1, 4, num2, num2.length);
 console.log(num1);
