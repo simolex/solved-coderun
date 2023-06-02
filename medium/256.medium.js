31d2ae3459c01b4a582c9348995f4dfce70540f4
simolex <simolex@yandex.ru>
2023-06-03 01:15:51 +0300

    add 256 - ok, save 269

--- medium/256.medium.js
+++ medium/256.medium.js
@@ -1,17 +1,49 @@
 //const getIt()
 
 module.exports = function (content) {
-    // const describe = /^describe.*?(?=\n)\n((.+\n)+)\}\);.*?(?=\n)$/gim;
-    const end = /(?<=\s*?\}\);.*?\n)(\s*?\}\);.*?)(?=\n)/gim;
+    const openDescribe = /^(.*?\n)*.*?describe\(.*?(?=\n)\n/gim;
 
-    const endTag = end.exec(content);
-    // ваше решение
+    const closeDescribe = /^(?<=\s*?\}\);.*?\n)(\}\);)(?=\n)\n/gim;
 
-    return con;
+    const blockItTest = /^(\s*?it(\.skip)?\(.+(?=\n)\n)/gim;
+
+    const getStartEndPosition = (content, pattern) => {
+        const result = pattern.exec(content);
+        if (result === null) return false;
+        return { start: result.index, end: pattern.lastIndex, content: result[0] };
+    };
+
+    const createItTests = (content, prefix, postfix, pattern) => {
+        const tests = [];
+        let prevIndex = 0;
+        let finded = false;
+        while ((result = pattern.exec(content)) !== null) {
+            console.log("eee=>", result, "eef=>", pattern.lastIndex);
+            finded = true;
+            currentIndex = result.index;
+            if (currentIndex != prevIndex) {
+                tests.push(`${prefix}${content.substring(prevIndex, currentIndex).trimEnd("\n")}\n${postfix}`);
+            }
+            prevIndex = currentIndex;
+        }
+        if (finded) {
+            tests.push(`${prefix}${content.substring(prevIndex)}${postfix}`);
+        }
+        return tests;
+    };
+
+    const openDescribeResult = getStartEndPosition(content, openDescribe);
+    const closeDescribeResult = getStartEndPosition(content, closeDescribe);
+
+    const testsListText = content.substring(openDescribeResult.end, closeDescribeResult.start);
+
+    return createItTests(testsListText, openDescribeResult.content, closeDescribeResult.content, blockItTest);
 };
 
 /**
- *  /describe.*?(?=\n)/gi - старт describe
+ *  /^describe.*?(?=\n)\n((.+\n)+)\}\);.*?(?=\n)$/gim - only it-pull
+ *  /describe.*?(?=\n)/gmi - старт describe
  *  /^\s*it(\.skip)?.+(?=\n)$/gim - test start
  *  /^\s*?\}\);.*?(?=\n)$/gmi - end test
  */
+// const blockItTest = /^(?<test>\s*it(\.skip)?.+(?=\n)\n(.*\n)*?(\s*?\}\);.*?)(?=\n)$)/gim;
--- medium/run256.js
+++ medium/run256.js
@@ -1,18 +1,44 @@
 const decode = require("./256.medium.js");
 
 console.log(
-    decode(`// Some imports and comments
-const component = require('./component');
+    decode(`import { classNames } from './classNames';
 
-describe('suite', function () {
-    it('test1', function () {
-        // Some test 1
+describe('classNames', () => {
+    it('with only first param', () => {
+        expect(classNames('someClass')).toBe('someClass');
     });
-    it('test2', function () {
-        // Some test 2
+    it('with additional class', () => {
+        const expected = 'someClass additionalClass-1 additionalClass-2';
+        expect(classNames('someClass', {}, ['additionalClass-1', 'additionalClass-2'])).toBe(
+            expected,
+        );
     });
-    it.skip('test3', function () {
-        // Some test 3
+    it('with mods', () => {
+        const expected = 'someClass additionalClass-1 additionalClass-2 hovered scrollable';
+        expect(
+            classNames('someClass', { hovered: true, scrollable: true }, [
+                'additionalClass-1',
+                'additionalClass-2',
+            ]),
+        ).toBe(expected);
+    });
+    it.skip('with mods false', () => {
+        const expected = 'someClass additionalClass-1 additionalClass-2 hovered';
+        expect(
+            classNames('someClass', { hovered: true, scrollable: false }, [
+                'additionalClass-1',
+                'additionalClass-2',
+            ]),
+        ).toBe(expected);
+    });
+    it('with mods undefined', () => {
+        const expected = 'someClass additionalClass-1 additionalClass-2 hovered';
+        expect(
+            classNames('someClass', { hovered: true, scrollable: undefined }, [
+                'additionalClass-1',
+                'additionalClass-2',
+            ]),
+        ).toBe(expected);
     });
 });
 `)
