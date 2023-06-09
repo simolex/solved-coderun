4beaf426c102b70f9d3517d95434b1a8d3d6ae32
simolex <simolex@yandex.ru>
2023-06-09 15:58:59 +0300

    save 321

--- medium/321/script.js
+++ medium/321/script.js
@@ -6,9 +6,16 @@
  * @return {number | null} искомое значение `font-variation-settings` (целое число) или null, если текст вписать нельзя
  */
 function calcFontVariationSettings(container, str, min, max) {
+    if (max === undefined || min === undefined || str === undefined) return null;
+
+    min = +min;
+    max = +max;
+    if (max < 1 || str.length === 0 || !container) return null;
     const textWrapper = document.createElement("span");
-    textWrapper.innerHTML = str;
+    textWrapper.textContent = str;
     textWrapper.style.display = "inline-block";
+    textWrapper.visibility = "hidden";
+    textWrapper.style.whiteSpace = "nowrap";
     container.appendChild(textWrapper);
 
     function ContainerSize(container) {
@@ -19,11 +26,17 @@ function calcFontVariationSettings(container, str, min, max) {
 
         if (container.style.boxSizing === "border-box") {
             const borderWidth =
-                parseFloat(contStyle.borderRightWidth) || 0 + parseFloat(contStyle.borderLeftWidth) || 0;
+                parseFloat(contStyle.borderRightWidth) ||
+                0 + parseFloat(contStyle.borderLeftWidth) ||
+                0;
             const borderHeight =
-                parseFloat(contStyle.borderBottomWidth) || 0 + parseFloat(contStyle.borderTopWidth) || 0;
-            const paddingWidth = parseFloat(contStyle.paddingRight) || 0 + parseFloat(contStyle.paddingLeft) || 0;
-            const paddingHeight = parseFloat(contStyle.paddingBottom) || 0 + parseFloat(contStyle.paddingTop) || 0;
+                parseFloat(contStyle.borderBottomWidth) ||
+                0 + parseFloat(contStyle.borderTopWidth) ||
+                0;
+            const paddingWidth =
+                parseFloat(contStyle.paddingRight) || 0 + parseFloat(contStyle.paddingLeft) || 0;
+            const paddingHeight =
+                parseFloat(contStyle.paddingBottom) || 0 + parseFloat(contStyle.paddingTop) || 0;
 
             const wrapWidthSize = borderWidth + paddingWidth;
             const wrapHeigthSize = borderHeight + paddingHeight;
@@ -42,27 +55,66 @@ function calcFontVariationSettings(container, str, min, max) {
 
     const isInsize = (el) => {
         const rect = el.getBoundingClientRect();
+        let wdth = rect.width - Parent.getWidth();
+        let hght = rect.height <= Parent.getHeight();
 
-        console.log({ elW: rect.width, pW: Parent.getWidth(), elH: rect.height, pH: Parent.getHeight() });
-
-        if (rect.width <= Parent.getWidth() && rect.height <= Parent.getHeight()) return true;
-        return false;
+        return { wdth, hght };
     };
-    // console.log((textWrapper.style.fontVariationSettings = `'wdth' ${4}`));
-    // isInsize(textWrapper);
-    // console.log((textWrapper.style.fontVariationSettings = `'wdth' ${150}`));
-    // isInsize(textWrapper);
+
+    if (min < 1) min = 1;
+    if (max > 150) max = 150;
+    let minInsize, maxInsize;
+
+    textWrapper.style.fontVariationSettings = `'wdth' ${min}`;
+    minInsize = isInsize(textWrapper);
+
+    if (!minInsize.hght) {
+        container.removeChild(textWrapper);
+        return null;
+    }
+    if (minInsize.wdth > 0) {
+        container.removeChild(textWrapper);
+        return null;
+    }
+
+    textWrapper.style.fontVariationSettings = `'wdth' ${max}`;
+    maxInsize = isInsize(textWrapper);
+
+    if (maxInsize.wdth < -0.2) {
+        container.removeChild(textWrapper);
+        return null;
+    }
+
+    // console.log(minInsize, maxInsize);
 
     let current = max;
+    let currentMin = min;
+    let currentMax = max;
     do {
+        current = currentMin + Math.floor((currentMax - currentMin) / 1.99);
         textWrapper.style.fontVariationSettings = `'wdth' ${current}`;
-        current -= 1;
-    } while (!isInsize(textWrapper) && current >= min - 1);
-    current++;
-    return current >= min ? current : false;
+
+        currentInsize = isInsize(textWrapper);
+        if (currentInsize.wdth < 0) {
+            minInsize = currentInsize;
+            currentMin = current;
+        } else {
+            maxInsize = currentInsize;
+            currentMax = current;
+        }
+    } while (
+        Math.abs(Math.abs(minInsize.wdth) + Math.abs(maxInsize.wdth)) > 0.1 &&
+        Math.floor((currentMax - currentMin) / 1.99) !== 0
+    );
+
+    container.removeChild(textWrapper);
+    container.appendChild(document.createTextNode(str));
+    container.style.fontVariationSettings = `'wdth' ${current}`;
+
+    return current;
 }
 
 document.fonts.onloadingdone = () => {
     console.log("Font loading complete");
-    console.log(calcFontVariationSettings(document.getElementById("container"), `DEMO DEMO DEMO  DEMO`, 0, 150));
+    console.log(calcFontVariationSettings(document.getElementById("container"), `DEMO`, 0, 150));
 };
