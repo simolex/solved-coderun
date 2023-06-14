bc623ca58e7a3c3ed4c9c6ad3188ace213edb828
simolex <simolex@yandex.ru>
2023-06-14 15:27:32 +0300

    solved 192 - OK

--- medium/192/index.js
+++ medium/192/index.js
@@ -2,7 +2,7 @@ const Band = {
     type: "band",
     name: "", //: string;
     friends: [], //: Band[];
-    genres: [] //: Genre[];
+    genres: [], //: Genre[];
 };
 
 const Genre = {
@@ -10,7 +10,7 @@ const Genre = {
     name: "", //: string;
     bands: [], //: Band[];
     subgenres: [], //: Genre[];
-    parent: null //: Genre | null;
+    parent: null, //: Genre | null;
 };
 
 /**
@@ -32,8 +32,10 @@ module.exports = function (data) {
     while (stackPath.length > 0) {
         currentObject = stackPath.pop();
         currentObject.vizited = "1";
-        findedObjects.push(currentObject);
-        if (currentObject.type === "band") {
+
+        if (currentObject && currentObject.type === "band") {
+            findedObjects.push(currentObject);
+
             if (currentObject.friends.length > 0) {
                 currentObject.friends.forEach((val) => addToPath(val));
             }
@@ -41,7 +43,10 @@ module.exports = function (data) {
                 currentObject.genres.forEach((val) => addToPath(val));
             }
         }
-        if (currentObject.type === "genre") {
+        if (currentObject && currentObject.type === "genre") {
+            if (currentObject.parent === null) {
+                findedObjects.push(currentObject);
+            }
             if (currentObject.bands.length > 0) {
                 currentObject.bands.forEach((val) => addToPath(val));
             }
@@ -56,8 +61,41 @@ module.exports = function (data) {
 
     findedObjects.sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0));
 
-    // TODO markdown output
+    let genreInMD = "";
+    let bandsInMD = "";
 
-    console.log("==>", findedObjects);
-    return data instanceof Band;
+    const getGenre = (input, obj, tabStr, level) => {
+        input += `${tabStr.repeat(level)}- ${obj.name}${obj.bands.length > 0 ? ": " : ""}${obj.bands
+            .map((n) => n.name)
+            .join(", ")}\n`;
+        let output =
+            input +
+            obj.subgenres
+                .sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0))
+                .reduce((out, sub) => out + getGenre("", sub, tabStr, level + 1), "");
+
+        return output;
+    };
+
+    findedObjects.forEach((obj) => {
+        if (obj.type === "genre" && obj.vizited !== "2") {
+            let record = getGenre("", obj, "  ", 0);
+            genreInMD += record;
+            obj.vizited = "2";
+        }
+
+        if (obj.type === "band" && obj.vizited !== "2") {
+            bandsInMD += `- ${obj.name}${obj.friends.length > 0 ? ", друзья: " : ""}${obj.friends
+                .sort((a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0))
+                .map((n) => n.name)
+                .join(", ")}\n`;
+            obj.vizited = "2";
+        }
+    });
+
+    const result = `${genreInMD.length > 0 ? `## Жанры\n\n${genreInMD}\n` : ""}${
+        bandsInMD.length > 0 ? `## Группы\n\n${bandsInMD}` : ""
+    }`;
+
+    return result;
 };
