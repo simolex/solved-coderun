c4913cb07fce4054dfe09c06d0c5b6368bfa09fc
simolex <simolex@yandex.ru>
2023-06-22 22:51:47 +0300

    add 365

---
+++ hard/365/index.js
@@ -0,0 +1,30 @@
+// export type TextNode = {
+//     type: 'TEXT';
+//     text: string;
+// }
+
+// export type ElementNode = {
+//     type: 'ELEMENT'
+//     styles: Record<string, string>;
+//     tag: string;
+//     children: Array<ElementNode | TextNode>;
+// }
+
+// export type HtmlNode = ElementNode | TextNode;
+
+// export type CssRule = {
+//   selector: string;
+//   declarations: Record<string, string>;
+// }
+
+// module.exports = function (html: HtmlNode, css: Array<CssRule>): HtmlNode {};
+/**
+ *
+ * @param {HtmlNode} html
+ * @param {Array<CssRule>} css
+ * @returns {HtmlNode}
+ */
+
+module.exports = function (html, css) {
+    return "";
+};
