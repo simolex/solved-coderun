3a0e29e44241856b2ac1a04484ae99271652d0e0
simolex <simolex@yandex.ru>
2023-07-13 20:58:44 +0300

    add 049.hard

---
+++ hard/049/index.js
@@ -0,0 +1,5 @@
+const a = (s) => {
+    const f = "geo";
+    return s.split("").reduce((sum, v, i) => sum + Math.abs(f.charCodeAt(i) - v.charCodeAt(0)), 0);
+};
+module.exports = function (pass) {};
