7bb77bcec416b71392a4aa0e74d58de495cf65e4
simolex <simolex@yandex.ru>
2024-04-07 11:51:42 +0300

    testing 49 task

--- notSolved/049/index.js
+++ notSolved/049/index.js
@@ -1,7 +1,46 @@
-const a = (s) => {
-    const f = "geo";
-    return s.split("").reduce((sum, v, i) => sum + Math.abs(f.charCodeAt(i) - v.charCodeAt(0)), 0);
-};
-module.exports = function (pass) {};
-
 //((x-"a")-(x-"z")+27)/2
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    crlfDelay: Infinity,
+    prompt: ""
+});
+
+let start = false;
+let curLine;
+
+rl.on("line", (line) => {
+    curLine = line.toString().trim();
+    if (!start) {
+        const n = readInt(curLine);
+        if (n !== 7) {
+            throw Error("is not test 1");
+        }
+        process.stdout.write("aaaaaaa\n");
+        process.stdout.write("");
+        // process.stdout.write("aaaaaaa");
+        // process.stdout.write("\n", () => {
+        start = true;
+        // });
+    } else {
+        if (readInt(curLine) === 79) {
+            process.stdout.write("georgiy\n");
+            process.stdout.write("");
+
+            // process.stdout.write("georgiy");
+            // process.stdout.write("\n", () => {});
+        }
+        if (readInt(curLine) === 0) {
+            rl.close();
+        }
+    }
+}).on("close", () => {
+    // process.exit(0);
+});
+
+function readInt(line) {
+    const n = Number(line);
+    return n;
+}
