d8b10e0d9de3177a6f84b2bcd7933557335737ca
simolex <simolex@yandex.ru>
2024-05-08 20:03:05 +0300

    remove old solving 26

--- notSolved/026/index.test.js
+++
@@ -1,8 +0,0 @@
-const setStyles = require(".");
-
-describe("026. Apply styles", () => {
-    test("test", () => {
-        const result = setStyles(htmlSet0, cssSet0);
-        expect(result).toEqual(resultSet0);
-    });
-});
