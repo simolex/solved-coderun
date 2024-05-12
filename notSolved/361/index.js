c5977f8c7837bbf3b898762326838a8d5b4cd8bc
simolex <simolex@yandex.ru>
2024-05-12 23:10:16 +0300

    fix

--- notSolved/361/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (countNails, nailPositions) {
-    nailPositions.sort((a, b) => a - b);
-
-    console.log(nailPositions);
-};
--- notSolved/361/index.test.js
+++
@@ -1,8 +0,0 @@
-const solution = require(".");
-
-describe("001. Pins", () => {
-    test("test", () => {
-        const result = solution(6, [3, 13, 12, 4, 14, 6]);
-        expect(result).toBe(5);
-    });
-});
