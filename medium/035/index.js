2cbab3d4fea57cdee9a9d744361107bdc85ccc70
simolex <simolex@yandex.ru>
2024-05-17 11:14:59 +0300

    done task 35

---
+++ medium/035/index.js
@@ -0,0 +1,106 @@
+/**
+ * 35. Движение по полосам
+ */
+
+function traffic(m, n) {
+    const dp = [];
+
+    dp.push(Array(n).fill(1n));
+
+    for (let i = 1; i < m; i++) {
+        dp.push(Array(n).fill(1n));
+
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + dp[i - 1][j - 1];
+        }
+    }
+
+    return dp[m - 1][n - 1].toString();
+}
+
+const _readline = require("readline");
+
+const _reader = _readline.createInterface({
+    input: process.stdin,
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on("line", (line) => {
+    _inputLines.push(line);
+});
+
+process.stdin.on("end", solve);
+
+function solve() {
+    const params = readArray();
+    const m = params[0];
+    const n = params[1];
+
+    const result = traffic(m, n);
+
+    console.log(result);
+}
+
+function readAllString() {
+    var arr = _inputLines.map((line) => line.trim());
+
+    return arr;
+}
+
+function readString() {
+    var str = _inputLines[_curLine].trim(" ");
+    _curLine++;
+    return str;
+}
+
+function readInt() {
+    const n = Number(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+function readBigInt() {
+    const n = BigInt(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+
+function readArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .filter((str) => str && str.length > 0)
+        .map((num) => Number(num));
+    _curLine++;
+    return arr;
+}
+
+function readBigIntArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .map((num) => BigInt(num));
+    _curLine++;
+    return arr;
+}
+
+function readStringArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .filter((str) => str && str.length > 0);
+    _curLine++;
+    return arr;
+}
+
+function readEdges(n) {
+    let grid = [];
+    for (let i = 0; i < n; i++) {
+        let vertex = readArray();
+        grid.push(vertex);
+    }
+    return grid;
+}
+
+module.exports = traffic;
---
+++ medium/035/index.test.js
@@ -0,0 +1,8 @@
+const solution = require(".");
+
+describe("35. Движение по полосам", () => {
+    test("test-1", () => {
+        const result = solution(4, 2);
+        expect(result).toBe("7");
+    });
+});
