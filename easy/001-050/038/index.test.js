040301588dcd90c52bf57c3f679e9bf3d6b46190
simolex <simolex@yandex.ru>
2024-05-19 17:45:09 +0300

    fix test

--- easy/001-050/038/index.test.js
+++ easy/001-050/038/index.test.js
@@ -2,7 +2,7 @@ const solution = require(".");
 
 describe("38. Площадь комнаты", () => {
     test("test-1", () => {
-        const result = solution(3, 2, (2)[("***", "*.*", "***")]);
+        const result = solution(3, 2, 2, ["***", "*.*", "***"]);
         expect(result).toBe(1);
     });
 });
