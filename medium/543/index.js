d66270a2891228995ca5cfa2c9486ecd995114a0
simolex <simolex@yandex.ru>
2024-08-29 23:50:21 +0300

    done task 543

---
+++ medium/543/index.js
@@ -0,0 +1,104 @@
+/**
+ * 543. Классы подобия треугольников
+ */
+
+const _readline = require("readline");
+
+const _reader = _readline.createInterface({
+    input: process.stdin
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on("line", (line) => {
+    _inputLines.push(line);
+});
+
+process.stdin.on("end", solve);
+
+function solve() {
+    const n = readInt();
+    const similarity = new Set();
+
+    let a, b, c;
+    let ab, ac, bc;
+    let triangle;
+    for (let i = 0; i < n; i++) {
+        triangle = readArray();
+
+        a = triangle[0];
+        b = triangle[1];
+        c = triangle[2];
+
+        ab = a + b;
+        ac = a + c;
+        bc = b + c;
+
+        let hash = ab / c + ac / b + bc / a;
+
+        similarity.add(hash);
+    }
+
+    console.log(similarity.size);
+}
+
+function readAllString() {
+    var arr = _inputLines.map((line) => line.trim());
+
+    return arr;
+}
+
+function readString() {
+    var str = _inputLines[_curLine].trim(" ");
+    _curLine++;
+    return str;
+}
+
+function readInt() {
+    const n = Number(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+function readBigInt() {
+    const n = BigInt(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+
+function readArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .filter((str) => str && str.length > 0)
+        .map((num) => Number(num));
+    _curLine++;
+    return arr;
+}
+
+function readBigIntArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .map((num) => BigInt(num));
+    _curLine++;
+    return arr;
+}
+
+function readStringArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .filter((str) => str && str.length > 0);
+    _curLine++;
+    return arr;
+}
+
+function readEdges(n) {
+    let grid = [];
+    for (let i = 0; i < n; i++) {
+        let vertex = readArray();
+        grid.push(vertex);
+    }
+    return grid;
+}
