30ba2bfe0cdde45206bcd6e5581c6f93eb272ec1
Alex S. <simolex@yandex.ru>
2025-08-16 00:29:20 +0300

    done task 5206

---
+++ easy/xxxx/5206/index.js
@@ -0,0 +1,104 @@
+/**
+ * 5206. Выставление тегов
+ */
+
+function getTagsCount(n) {
+    const buffer = [1, 1, 0]
+
+    if (n < 3) return n;
+
+    let current;
+    let count = 2;
+    for (let i = 2; i < n; i++) {
+        current = buffer[(i + 1) % 3] + buffer[(i + 2) % 3];
+        buffer[i % 3] = current;
+        count += current;
+
+    }
+    return count;
+}
+
+const _readline = require("readline");
+
+const _reader = _readline.createInterface({
+    input: process.stdin,
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on("line", (line) => {
+    _inputLines.push(line);
+});
+
+process.stdin.on("end", solve);
+
+function solve() {
+    const n = readInt();
+
+    const result = getTagsCount(n);
+
+    console.log(result);
+}
+
+function readAllString() {
+    var arr = _inputLines.map((line) => line.trim());
+
+    return arr;
+}
+
+function readString() {
+    var str = _inputLines[_curLine].trim(" ");
+    _curLine++;
+    return str;
+}
+
+function readInt() {
+    const n = Number(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+function readBigInt() {
+    const n = BigInt(_inputLines[_curLine]);
+    _curLine++;
+    return n;
+}
+
+function readArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .filter((str) => str && str.length > 0)
+        .map((num) => Number(num));
+    _curLine++;
+    return arr;
+}
+
+function readBigIntArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .map((num) => BigInt(num));
+    _curLine++;
+    return arr;
+}
+
+function readStringArray() {
+    var arr = _inputLines[_curLine]
+        .trim(" ")
+        .split(" ")
+        .filter((str) => str && str.length > 0);
+    _curLine++;
+    return arr;
+}
+
+function readEdges(n) {
+    let grid = [];
+    for (let i = 0; i < n; i++) {
+        let vertex = readArray();
+        grid.push(vertex);
+    }
+    return grid;
+}
+
+module.exports = getTagsCount;
